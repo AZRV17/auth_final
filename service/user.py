55f2543345e183f194c4606ba6f8dfec541ef70f
AZAR <alex170805azrov@gmail.com>
2025-12-15 18:07:08 +0300

    add some try/except constructions

--- service/user.py
+++ service/user.py
@@ -13,304 +13,345 @@ LOG_FILE = "app.log"
 ROLES = ['Администратор', 'Клиент', 'Менеджер', 'Оператор']
 
 def login():
-    login = input("Введите логин: ")
-    password = input("Введите пароль: ")
-
     conn = get_connection()
     cur = conn.cursor()
+    try:
+        login = input("Введите логин: ")
+        password = input("Введите пароль: ")
 
-    password_hash, role = cur.execute("""
-                SELECT password, role
-                FROM users
-                WHERE login = ?
-                """, (login,)).fetchone()
-
-    if not verify_password(password, password_hash):
-        print("Неправильный логин или пароль")
-
-    if role:
-        cur.execute("""
-                    UPDATE users
-                    SET is_active=1
+        password_hash, role = cur.execute("""
+                    SELECT password, role
+                    FROM users
                     WHERE login = ?
-                    """, (login,))
-        conn.commit()
+                    """, (login,)).fetchone()
 
-        log_action(f"Успешный вход пользователя: {login}")
-        log_action(f"LOGIN {login} role={role[0]}")
-        print(f"Вход выполнен: {login}")
-    else:
-        log_action(f"Неудачный вход пользователя: {login}")
-        print("Неверный логин или пароль.")
+        if not verify_password(password, password_hash):
+            print("Неправильный логин или пароль")
+
+        if role:
+            cur.execute("""
+                        UPDATE users
+                        SET is_active=1
+                        WHERE login = ?
+                        """, (login,))
+            conn.commit()
+
+            log_action(f"Успешный вход пользователя: {login}")
+            log_action(f"LOGIN {login} role={role[0]}")
+            print(f"Вход выполнен: {login}")
+        else:
+            log_action(f"Неудачный вход пользователя: {login}")
+            print("Неверный логин или пароль.")
+    except:
+        print("Некорректный ввод")
     conn.close()
 
 def logout():
-    login = input("Введите логин для выхода: ")
-
     conn = get_connection()
     cur = conn.cursor()
+    try:
+        login = input("Введите логин для выхода: ")
 
-    role = cur.execute("SELECT role FROM users WHERE login=?", (login,)).fetchone()
-    if role:
-        cur.execute("""
-                    UPDATE users
-                    SET is_active=0
-                    WHERE login = ?
-                    """, login)
-        conn.commit()
-        print(f"Пользователь {login} вышел из системы.")
-        log_action(f"Выход из системы: {login}")
-        log_action(f"LOGOUT {login} role={role[0]}")
-    else:
-        print("Логин не найден.")
+        role = cur.execute("SELECT role FROM users WHERE login=?", (login,)).fetchone()
+        if role:
+            cur.execute("""
+                        UPDATE users
+                        SET is_active=0
+                        WHERE login = ?
+                        """, login)
+            conn.commit()
+            print(f"Пользователь {login} вышел из системы.")
+            log_action(f"Выход из системы: {login}")
+            log_action(f"LOGOUT {login} role={role[0]}")
+        else:
+            print("Логин не найден.")
+    except:
+        print("Некорректный ввод")
     conn.close()
 
 def register():
     conn = get_connection()
     cur = conn.cursor()
 
-    login = input("Введите логин: ")
-    email = input("Введите email: ").strip()
-
-    cur.execute("SELECT 1 FROM users WHERE login=? OR email=?", (login, email))
-    if cur.fetchone():
-        print("Логин или email уже существует")
-        conn.close()
-        return
-
-    name = input("Введите имя: ")
-    surname = input("Введите фамилию: ")
-    role = input("Введите роль (по умолчанию Клиент): ") or "Клиент"
-    if role not in ROLES:
-        print("Неверная роль (Администратор, Клиент, Менеджер, Оператор)")
-        conn.close()
-        return
-
-    while True:
-        password = input("Введите пароль (a для генерации пароля): ").strip()
-        if password == "a":
-            password = generate_password()
-        elif len(password) < 6:
-            print("Пароль должен содрежать минимум 6 символов")
-            continue
-        break
-
-    password_hash = hash_password(password)
-
     try:
-        code = send_email(email)
-        print("Код отправлен на почту")
-    except:
-        print("Регистрация прервана из-за ошибки отправки email")
-        log_action(f"EMAIL_ERROR email={email}")
-        conn.close()
-        return
+        login = input("Введите логин: ")
+        email = input("Введите email: ").strip()
 
-    confirm = input("Введите код, полученный по почте: ")
-    if confirm != code:
-        print("Неверный код. Регистрация отменена")
-        log_action(f"Неудачная регистрация email={email}")
-        conn.close()
-        return
+        cur.execute("SELECT 1 FROM users WHERE login=? OR email=?", (login, email))
+        if cur.fetchone():
+            print("Логин или email уже существует")
+            conn.close()
+            return
 
-    cur.execute("""
-                INSERT INTO users
-                (first_name, last_name, login, email, password, registered_at, role, is_active)
-                VALUES (?, ?, ?, ?, ?, ?, ?, 0)
-                """, (
-                    name, surname, login, email,
-                    password_hash, datetime.now().strftime("%Y-%m-%d"), role
-                ))
+        name = input("Введите имя: ")
+        surname = input("Введите фамилию: ")
+        role = input("Введите роль (по умолчанию Клиент): ") or "Клиент"
+        if role not in ROLES:
+            print("Неверная роль (Администратор, Клиент, Менеджер, Оператор)")
+            conn.close()
+            return
 
-    conn.commit()
-    conn.close()
-    print(f"Регистрация успешна! Ваш пароль: {password}")
-    log_action(f"Успешная регистрация: {login}")
-
-def change_password():
-    login = input("Введите логин: ")
-    conn = get_connection()
-    cur = conn.cursor()
-
-    user_id = cur.execute("SELECT id FROM users WHERE login=?", (login,)).fetchone()
-
-    if user_id:
         while True:
-            new_pass = input("Введите новый пароль (a для генерации пароля): ").strip()
-            if new_pass == "a":
-                new_pass = generate_password()
-            elif len(new_pass) < 6:
+            password = input("Введите пароль (a для генерации пароля): ").strip()
+            if password == "a":
+                password = generate_password()
+            elif len(password) < 6:
                 print("Пароль должен содрежать минимум 6 символов")
                 continue
             break
 
-        password_hash = hash_password(new_pass)
+        password_hash = hash_password(password)
+
+        try:
+            code = send_email(email)
+            print("Код отправлен на почту")
+        except:
+            print("Регистрация прервана из-за ошибки отправки email")
+            log_action(f"EMAIL_ERROR email={email}")
+            conn.close()
+            return
+
+        confirm = input("Введите код, полученный по почте: ")
+        if confirm != code:
+            print("Неверный код. Регистрация отменена")
+            log_action(f"Неудачная регистрация email={email}")
+            conn.close()
+            return
 
         cur.execute("""
-                    UPDATE users
-                    SET password=?
-                    WHERE login = ?
-                    """, (password_hash, login))
+                    INSERT INTO users
+                    (first_name, last_name, login, email, password, registered_at, role, is_active)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, 0)
+                    """, (
+                        name, surname, login, email,
+                        password_hash, datetime.now().strftime("%Y-%m-%d"), role
+                    ))
 
         conn.commit()
-        conn.close()
-        print(f"Новый пароль: {new_pass}")
-        log_action(f"Успешная смена пароля: {login}")
-    else:
-        print("Пользователь не найден.")
+        print(f"Регистрация успешна! Ваш пароль: {password}")
+        log_action(f"Успешная регистрация: {login}")
+    except:
+        print("Некорректный ввод")
+
+    conn.close()
+
+def change_password():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    try:
+        login = input("Введите логин: ")
+
+        user_id = cur.execute("SELECT id FROM users WHERE login=?", (login,)).fetchone()
+
+        if user_id:
+            while True:
+                new_pass = input("Введите новый пароль (a для генерации пароля): ").strip()
+                if new_pass == "a":
+                    new_pass = generate_password()
+                elif len(new_pass) < 6:
+                    print("Пароль должен содрежать минимум 6 символов")
+                    continue
+                break
+
+            password_hash = hash_password(new_pass)
+
+            cur.execute("""
+                        UPDATE users
+                        SET password=?
+                        WHERE login = ?
+                        """, (password_hash, login))
+
+            conn.commit()
+            print(f"Новый пароль: {new_pass}")
+            log_action(f"Успешная смена пароля: {login}")
+        else:
+            print("Пользователь не найден.")
+    except:
+        print("Некорректный ввод")
+
+    conn.close()
 
 def edit_user():
-    login = input("Введите логин пользователя: ")
-
     conn = get_connection()
     cur = conn.cursor()
 
-    cur.execute("SELECT id FROM users WHERE login=?", (login,))
-    if not cur.fetchone():
-        print("Пользователь не найден")
-        conn.close()
-        return
+    try:
+        login = input("Введите логин пользователя: ")
 
-    name = input("Новое имя (Enter — пропустить): ")
-    surname = input("Новая фамилия (Enter — пропустить): ")
-    role = input("Новая роль (Enter — пропустить): ")
+        cur.execute("SELECT id FROM users WHERE login=?", (login,))
+        if not cur.fetchone():
+            print("Пользователь не найден")
+            conn.close()
+            return
 
-    if name:
-        cur.execute("UPDATE users SET first_name=? WHERE login=?", (name, login))
-    if surname:
-        cur.execute("UPDATE users SET last_name=? WHERE login=?", (surname, login))
-    if role:
-        cur.execute("UPDATE users SET role=? WHERE login=?", (role, login))
+        name = input("Новое имя (Enter — пропустить): ")
+        surname = input("Новая фамилия (Enter — пропустить): ")
+        role = input("Новая роль (Enter — пропустить): ")
 
-    conn.commit()
+        if name:
+            cur.execute("UPDATE users SET first_name=? WHERE login=?", (name, login))
+        if surname:
+            cur.execute("UPDATE users SET last_name=? WHERE login=?", (surname, login))
+        if role:
+            cur.execute("UPDATE users SET role=? WHERE login=?", (role, login))
+
+        conn.commit()
+
+        print("Данные обновлены")
+        log_action(f"Изменение данных пользователя: {login}")
+    except:
+        print("Некорректный ввод")
+
     conn.close()
-
-    print("Данные обновлены")
-    log_action(f"Изменение данных пользователя: {login}")
 
 def search():
-    term = input("Введите имя, роль или статус (True/False): ").lower()
-
     conn = get_connection()
-    cur = conn.cursor()
 
-    cur.execute("""
-        SELECT * FROM users
-        WHERE lower(first_name) LIKE ?
-           OR lower(last_name) LIKE ?
-           OR lower(role) LIKE ?
-           OR CAST(is_active AS TEXT) LIKE ?
-    """, (f"%{term}%", f"%{term}%", f"%{term}%", f"%{term}%"))
+    try:
+        term = input("Введите имя, роль или статус (True/False): ").lower()
+        cur = conn.cursor()
 
-    rows = cur.fetchall()
+        cur.execute("""
+            SELECT * FROM users
+            WHERE lower(first_name) LIKE ?
+               OR lower(last_name) LIKE ?
+               OR lower(role) LIKE ?
+               OR CAST(is_active AS TEXT) LIKE ?
+        """, (f"%{term}%", f"%{term}%", f"%{term}%", f"%{term}%"))
+
+        rows = cur.fetchall()
+        conn.close()
+
+        if not rows:
+            print("Ничего не найдено")
+        else:
+            for r in rows:
+                print(r)
+    except:
+        print("Некорректный ввод")
+
     conn.close()
-
-    if not rows:
-        print("Ничего не найдено")
-    else:
-        for r in rows:
-            print(r)
 
 def filter_users():
-    role = input("Введите роль (Enter — пропустить): ")
-    status = input("Введите статус (True/False или Enter): ")
-
-    query = "SELECT * FROM users WHERE 1=1"
-    params = []
-
-    if role:
-        query += " AND role=?"
-        params.append(role)
-
-    if status.lower() in ["true", "false"]:
-        query += " AND is_active=?"
-        params.append(1 if status.lower() == "true" else 0)
-
     conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(query, params)
 
-    rows = cur.fetchall()
+    try:
+        role = input("Введите роль (Enter — пропустить): ")
+        status = input("Введите статус (True/False или Enter): ")
+
+        query = "SELECT * FROM users WHERE 1=1"
+        params = []
+
+        if role:
+            query += " AND role=?"
+            params.append(role)
+
+        if status.lower() in ["true", "false"]:
+            query += " AND is_active=?"
+            params.append(1 if status.lower() == "true" else 0)
+
+        cur = conn.cursor()
+        cur.execute(query, params)
+
+        rows = cur.fetchall()
+        conn.close()
+
+        if not rows:
+            print("Совпадений нет")
+        else:
+            for r in rows:
+                print(r)
+    except:
+        print("Некорректный ввод")
+
     conn.close()
-
-    if not rows:
-        print("Совпадений нет")
-    else:
-        for r in rows:
-            print(r)
 
 def mass_status():
-    new_status = input("Введите новый статус для всех (True/False): ").lower()
-
-    if new_status not in ["true", "false"]:
-        print("Некорректный ввод")
-        return
-
-    if new_status == "true":
-        status_value = 1
-    else:
-        status_value = 0
-
     conn = get_connection()
-    cur = conn.cursor()
 
-    cur.execute("UPDATE users SET is_active=?", (status_value,))
-    conn.commit()
+    try:
+        new_status = input("Введите новый статус для всех (True/False): ").lower()
+
+        if new_status not in ["true", "false"]:
+            print("Некорректный ввод")
+            return
+
+        if new_status == "true":
+            status_value = 1
+        else:
+            status_value = 0
+
+        cur = conn.cursor()
+
+        cur.execute("UPDATE users SET is_active=?", (status_value,))
+        conn.commit()
+        conn.close()
+
+        log_action(f"Успешное изменение статусов")
+        print("Статусы всех пользователей изменены")
+    except:
+        print("Некорректный ввод")
     conn.close()
-
-    log_action(f"Успешное изменение статусов")
-    print("Статусы всех пользователей изменены")
 
 def logins_with_surname():
     conn = get_connection()
     cur = conn.cursor()
 
-    cur.execute("""
-                SELECT login, last_name
-                FROM users
-                WHERE lower(login) LIKE '%' || lower(last_name) || '%'
-                """)
+    try:
+        cur.execute("""
+                    SELECT login, last_name
+                    FROM users
+                    WHERE lower(login) LIKE '%' || lower(last_name) || '%'
+                    """)
 
-    rows = cur.fetchall()
+        rows = cur.fetchall()
+
+        count = len(rows)
+
+        print(f"Пользователей, чей логин содержит фамилию: {count}")
+
+        if count > 0:
+            for login, last_name in rows:
+                print(f"Логин: {login}, Фамилия: {last_name}")
+    except:
+        print("Некорректный ввод")
     conn.close()
-
-    count = len(rows)
-
-    print(f"Пользователей, чей логин содержит фамилию: {count}")
-
-    if count > 0:
-        for login, last_name in rows:
-            print(f"Логин: {login}, Фамилия: {last_name}")
 
 def show_stats():
     conn = get_connection()
-    df = pd.read_sql_query("SELECT * FROM users", conn)
-    conn.close()
 
-    print(f"Активных пользователей: {df['is_active'].sum()}")
-    print("Пользователи по ролям:")
-    for role, count in df['role'].value_counts().items():
-        print(f"  {role}: {count}")
-    print("Повторяющиеся имена:")
-    repeated = df['first_name'].value_counts()
-    repeated = repeated[repeated > 1]
-
-    if repeated.empty:
-        print("  Нет повторяющихся имен")
-    else:
-        for name, count in repeated.items():
-            print(f"  {name}: {count}")
-    
-def export_csv():
     try:
-        conn = get_connection()
         df = pd.read_sql_query("SELECT * FROM users", conn)
         conn.close()
+
+        print(f"Активных пользователей: {df['is_active'].sum()}")
+        print("Пользователи по ролям:")
+        for role, count in df['role'].value_counts().items():
+            print(f"  {role}: {count}")
+        print("Повторяющиеся имена:")
+        repeated = df['first_name'].value_counts()
+        repeated = repeated[repeated > 1]
+
+        if repeated.empty:
+            print("  Нет повторяющихся имен")
+        else:
+            for name, count in repeated.items():
+                print(f"  {name}: {count}")
+    except:
+        print("Некорректный ввод")
+    conn.close()
+    
+def export_csv():
+    conn = get_connection()
+    try:
+        df = pd.read_sql_query("SELECT * FROM users", conn)
         df.to_csv('export/users.csv', index=False, encoding='utf-8')
         print("База данный успешно сохранена в export/users.csv")
         log_action("База данных сохранена в CSV")
     except Exception as e:
         print(f"Ошибка при сохранении: {e}")
+    conn.close()
 
 def get_active_users_by_role_from_logs(role_input):
     daily_counts = {}
@@ -337,13 +378,16 @@ def get_active_users_by_role_from_logs(role_input):
                         daily_counts[date] -= 1
 
     except FileNotFoundError:
-        print("Лог-файл не найден.")
+        print("Лог-файл не найден")
         return {}
 
     return daily_counts
 
 def print_all_users():
     conn = get_connection()
-    df = pd.read_sql_query("SELECT * FROM users", conn)
-    print(df)
+    try:
+        df = pd.read_sql_query("SELECT * FROM users", conn)
+        print(df)
+    except:
+        print("Ошибка при выводе пользователей")
     conn.close()
--- service/visualization.py
+++ service/visualization.py
@@ -9,64 +9,76 @@ from utils.logger import log_action
 
 
 def show(fig, filename):
-    os.makedirs("graphs", exist_ok=True)
-    fig.tight_layout()
-    path = f"graphs/{filename}"
-    fig.savefig(path)
-    print(f"График сохранён: {path}")
-    log_action(f"Сохрание графика: {path}")
-    plt.show()
+    try:
+        os.makedirs("graphs", exist_ok=True)
+        fig.tight_layout()
+        path = f"graphs/{filename}"
+        fig.savefig(path)
+        print(f"График сохранён: {path}")
+        log_action(f"Сохрание графика: {path}")
+        plt.show()
+    except:
+        print("Ошибка при отображении или сохранении графика")
 
 
 def users_by_role():
     conn = get_connection()
-    df = pd.read_sql_query("SELECT * FROM users", conn)
-    conn.close()
+    try:
+        df = pd.read_sql_query("SELECT * FROM users", conn)
+        conn.close()
 
-    role_counts = df['Роль'].value_counts()
+        role_counts = df['Роль'].value_counts()
 
-    fig = plt.figure()
-    role_counts.plot(kind='bar')
-    plt.title("Распределение пользователей по ролям")
-    plt.xlabel("Роль")
-    plt.ylabel("Количество")
+        fig = plt.figure()
+        role_counts.plot(kind='bar')
+        plt.title("Распределение пользователей по ролям")
+        plt.xlabel("Роль")
+        plt.ylabel("Количество")
 
-    show(fig, "users_by_role.png")
+        show(fig, "users_by_role.png")
+    except:
+        print("Ошибка при отрисковке графика")
 
 def registrations_by_date():
     conn = get_connection()
-    df = pd.read_sql_query("SELECT * FROM users", conn)
-    conn.close()
+    try:
+        df = pd.read_sql_query("SELECT * FROM users", conn)
+        conn.close()
 
-    df['Дата регистрации'] = df['Дата регистрации'].astype(str)
-    date_counts = df['Дата регистрации'].value_counts().sort_index()
+        df['Дата регистрации'] = df['Дата регистрации'].astype(str)
+        date_counts = df['Дата регистрации'].value_counts().sort_index()
 
-    fig = plt.figure()
-    date_counts.plot(kind='line', marker='o')
-    plt.title("Частота регистраций по датам")
-    plt.xlabel("Дата")
-    plt.ylabel("Количество регистраций")
+        fig = plt.figure()
+        date_counts.plot(kind='line', marker='o')
+        plt.title("Частота регистраций по датам")
+        plt.xlabel("Дата")
+        plt.ylabel("Количество регистраций")
 
-    show(fig, "registrations_by_date.png")
+        show(fig, "registrations_by_date.png")
+    except:
+        print("Ошибка при отрисковке графика")
 
 def active_users_by_role():
-    role = input("Введите роль: ").strip()
+    try:
+        role = input("Введите роль: ").strip()
 
-    data = get_active_users_by_role_from_logs(role)
+        data = get_active_users_by_role_from_logs(role)
 
-    if not data:
-        print("Нет данных для выбранной роли.")
-        return
+        if not data:
+            print("Нет данных для выбранной роли.")
+            return
 
-    dates = sorted(data.keys())
-    counts = [data[d] for d in dates]
+        dates = sorted(data.keys())
+        counts = [data[d] for d in dates]
 
-    fig = plt.figure(figsize=(10, 6))
-    plt.plot(dates, counts, marker='o')
+        fig = plt.figure(figsize=(10, 6))
+        plt.plot(dates, counts, marker='o')
 
-    plt.title(f"Активные пользователи по дням (роль: {role})")
-    plt.xlabel("Дата")
-    plt.ylabel("Количество активных пользователей")
-    plt.xticks(rotation=45, ha='right')
+        plt.title(f"Активные пользователи по дням (роль: {role})")
+        plt.xlabel("Дата")
+        plt.ylabel("Количество активных пользователей")
+        plt.xticks(rotation=45, ha='right')
 
-    show(fig, f"active_users_{role}.png")
+        show(fig, f"active_users_{role}.png")
+    except:
+        print("Ошибка при отрисковке графика")
